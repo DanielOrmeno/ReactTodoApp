e50c27c079fd6930ff7de2e649d27cbc9820382d
Erin B <erin@flyingfox.net.nz>
2018-10-16 08:45:33 +1000

    Added index.html and react files.

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = (props) => {
+        return (
+            <header className="top">
+                <h1>Awesome Todo App</h1>
+            </header>
+        )
+}
+
+
+
+export default Header;
